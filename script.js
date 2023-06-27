e92ad5030d3cccd83cdb8cecc4bf6f309305b23b
LukeBertus <vboxuser@Xubuntu2.myguest.virtualbox.org>
2023-06-27 15:31:59 +0800

    Add overlay on button press

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const body = document.querySelector("body")
+const newBook = document.querySelector(".newBook")
+const global = document.querySelector(".global")
+
+newBook.addEventListener('click', appendForm)
+
+function appendForm() {
+    let formContainer = document.createElement('div');
+    formContainer.classList.add("formContainer")
+    body.appendChild(formContainer);
+    global.classList.add("overlay");
+}
+
+
+
+
+
 let myLibrary = [];
 
 function Book() {
@@ -12,6 +29,6 @@ function displayBooks () {
 
 }
 
-function newBook() {
-    
+function generateBook() {
+
 }
